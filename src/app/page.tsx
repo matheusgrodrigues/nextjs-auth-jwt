774bdf923568ff6d0c48a7cfd42f6e1ce7e67db0
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2023-10-02 04:52:30 -0300

    feat: :sparkles: Criando a-avatar
    Criando a-avatar e mudando sass para css

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Avatar } from "./components";
+
+export default function Home () {
+  return (
+      <Avatar image="/images/a-avatar.jpeg" size="normal" shape="circle" />
+  );
+}
