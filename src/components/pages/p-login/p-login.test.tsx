5184625d53200320e2ed5b4952a6c3b1202edb31
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2023-10-04 11:50:24 -0300

    test: Adicionando teste de snapshot no p-login

--- src/components/pages/p-login/p-login.test.tsx
+++ src/components/pages/p-login/p-login.test.tsx
@@ -9,17 +9,17 @@ import { I_OLoginForm } from "@/components/organism/o-login-form/o-login-form";
 import { P_Home } from "./p-login";
 
 describe("Deve renderizar a pagina de login, corretamente", () => {
-  beforeEach(() => {
-    const loginTitleProps: I_OLoginTitle = {
-      image: "",
-      title: "",
-      username: "",
-    };
-    const loginFormProps: I_OLoginForm = {
-      onSubmit: () => null,
-      errors: {},
-    };
+  const loginTitleProps: I_OLoginTitle = {
+    image: "",
+    title: "",
+    username: "",
+  };
+  const loginFormProps: I_OLoginForm = {
+    onSubmit: () => null,
+    errors: {},
+  };
 
+  beforeEach(() => {
     render(
       <P_Home
         templateLoginProps={{
@@ -38,6 +38,22 @@ describe("Deve renderizar a pagina de login, corretamente", () => {
 
     // Assert
     expect(get_t_login).toBeInTheDocument();
+  });
+  // ============================================
+
+  it("Deve preservar a estrutura visual da p-login", () => {
+    const get_p_login = renderer
+      .create(
+        <P_Home
+          templateLoginProps={{
+            loginTitleProps,
+            loginFormProps,
+          }}
+        />
+      )
+      .toJSON();
+
+    expect(get_p_login).toMatchSnapshot();
   });
   // ============================================
 });
