a2c1025dc3254264f2f52d070959371b9e1407b9
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2023-10-06 16:47:54 -0300

    feat: :lipstick: Ajustando espaçamentos

--- src/components/templates/t-welcome/t-welcome.tsx
+++ src/components/templates/t-welcome/t-welcome.tsx
@@ -11,13 +11,15 @@ export interface I_TWelcome {
 export const T_Welcome = ({ username, handleLogout }: I_TWelcome) => {
   return (
     <main className={styles.t_welcome} id="t-welcome">
-      <A_Text data-testid="a-text-welcome" variant="fwSb-fs16-primary">
-        Bem-vindo(a) ao nosso serviço !
-      </A_Text>
-      <A_Title data-testid="a-title-username" variant="fwSB-fs48-lh60-lspN2-gray900">
-        {username}
-      </A_Title>
-      <A_Text data-testid="a-text-description" variant="fwReg-fs20-lh30-gray500">
+      <div className={styles.t_welcome__heading}>
+        <A_Text data-testid="a-text-welcome" variant="fwSb-fs16-primary">
+          Bem-vindo(a) ao nosso serviço !
+        </A_Text>
+        <A_Title data-testid="a-title-username" variant="fwSB-fs48-lh60-lspN2-gray900">
+          {username}
+        </A_Title>
+      </div>
+      <A_Text data-testid="a-text-description" variant="fwReg-fs20-lh30-gray500" id="a-text-description">
         Queremos que você se sinta em casa e aproveite ao máximo tudo o que oferecemos. Se tiver alguma dúvida ou
         precisar de ajuda, não hesite em nos chamar. Estamos sempre prontos para tornar sua experiência conosco
         incrível. Mais uma vez, bem-vindo(a)!
