bae191a43a85afe1b301f152d269b07d7795456a
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2023-10-16 10:15:30 -0300

    feat: :art: Refinando interface

--- src/components/pages/p-login/p-login.tsx
+++ src/components/pages/p-login/p-login.tsx
@@ -7,6 +7,9 @@ import { I_TLogin } from "@/components/templates/t-login/t-login";
 // Templates
 import { T_Login } from "@/components/templates";
 
+// Organism
+import { O_Header } from "@/components/organism/o-header/o-header";
+
 interface I_PHome {
   t_loginProps: I_TLogin;
 }
@@ -15,8 +18,12 @@ export const P_Home = ({ t_loginProps }: I_PHome) => {
   const { o_loginTitle, handleLoginForm } = t_loginProps;
 
   return (
-    <main data-testid="p-home" className={styles.p_home}>
-      <T_Login o_loginTitle={o_loginTitle} handleLoginForm={handleLoginForm} />
-    </main>
+    <>
+      <O_Header />
+
+      <main data-testid="p-home" className={styles.p_home}>
+        <T_Login o_loginTitle={o_loginTitle} handleLoginForm={handleLoginForm} />
+      </main>
+    </>
   );
 };
