f7cecb282846806c1aa5927de11eea11c2ef3eca
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-04-09 11:42:56 -0300

    Removendo comentarios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
 "use client";
 
-// Interfaces
 import { I_OLoginTitle } from "@/components/organism/o-login-title/o-login-title";
 import { I_OHeader } from "@/components/organism/o-header/o-header";
 import { I_TLogin } from "@/components/templates/t-login/t-login";
 
-// Handles
 import { handleLoginForm } from "@/components/organism/o-login-form/login-form-validation";
 
-// Organism
 import { O_BlockUI } from "@/components/organism/o-block-ui/o-block-ui";
 
-// Pages
 import { P_Home } from "@/components/pages/p-login/p-login";
 
-// HOC
 import { I_SessionHOC, withSessionHOC } from "@/services/sessionService/sessionService";
 
 interface I_Home extends I_SessionHOC {}
@@ -24,27 +19,21 @@ function Home({ data, loading, error }: I_Home) {
 
   const showBlockUI = session && !error && !loading ? true : false;
 
-  // Organism: Header
   const o_headerProps: I_OHeader = {
     link: "https://github.com/matheusgrodrigues",
     image: "/images/a-avatar.jpeg",
   };
 
-  // Dados falsos
-
-  // Organism: LoginTitle
   const o_loginTitle: I_OLoginTitle = {
     image: "/images/a-avatar.jpeg",
     title: "Acesse sua conta",
   };
 
-  // Template: LoginForm
   const t_loginProps: I_TLogin = {
     o_loginTitle,
     handleLoginForm,
   };
 
-  // Organism: Footer
   const o_footerProps = {
     name: "matheusgomesdev",
     site: "https://matheusgomesdev.com.br",
