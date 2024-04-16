c45d6f6e875db57318548aec0263af7b0195e957
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-04-16 09:59:32 -0300

    Atualizando mais testes

--- __tests__/src/app/page.test.tsx
+++ __tests__/src/app/page.test.tsx
@@ -5,24 +5,7 @@ import renderer from 'react-test-renderer';
 
 import { AppRouterContextProviderMock } from '@/core/utils/test-utils/app-router-ctx-provider';
 
-// import { AuthResponseSchema } from '@/schemas/AuthSchema';
-
 import Home from '@/app/page';
-
-/*
-const mockLoginResponse: AuthResponseSchema = {
-    jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjk3MTE1NzMxLCJleHAiOjE2OTk3MDc3MzF9.EmMz1jdfjdLB-F35CoRkSPaj71dBbwVTb3QEjxclmS8',
-    user: {
-        id: 1,
-        username: 'topete',
-        email: 'topete.matheus@gmail.com',
-        provider: 'local',
-        confirmed: true,
-        blocked: false,
-        createdAt: '2023-10-11T17:46:26.944Z',
-        updatedAt: '2023-10-11T22:31:06.753Z',
-    },
-}; */
 
 describe('Deve renderizar a pagina de login, corretamente.', () => {
     const push = jest.fn();
